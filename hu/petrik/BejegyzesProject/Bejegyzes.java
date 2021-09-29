38f2b72f3dbe8136809af9acc4fa2cf4be2d07c8
Mate501 <jurancsik.mate501@gmail.com>
2021-09-29 16:06:43 +0200

    Getter,Setter,toString,like++

--- hu/petrik/BejegyzesProject/Bejegyzes.java
+++ hu/petrik/BejegyzesProject/Bejegyzes.java
@@ -3,16 +3,51 @@ package hu.petrik.BejegyzesProject;
 import java.time.LocalDateTime;
 
 public class Bejegyzes {
-    String szerzo;
+     public String szerzo;
     String tartalom;
-    int likeok;
+    int likeok = 0;
     LocalDateTime letrejott;
     LocalDateTime szerkeztve;
 
+    public String getSzerzo() {
+        return szerzo;
+    }
+
+    public String getTartalom() {
+        return tartalom;
+    }
+
+    public void setTartalom(String tartalom) {
+        this.tartalom = tartalom;
+    }
+
+    public int getLikeok() {
+        return likeok;
+    }
+
+    public LocalDateTime getLetrejott() {
+        return letrejott;
+    }
+
+    public LocalDateTime getSzerkeztve() {
+        return szerkeztve;
+    }
+    public void like()
+    {
+        likeok++;
+    }
     public Bejegyzes(String szerzo, String tartalom) {
 
     }
 
-
-
+    @Override
+    public String toString() {
+        return "Bejegyzes{" +
+                "szerzo='" + szerzo + '-' +
+                ", likeok=" + likeok + '-' +
+                ", letrejott=" + letrejott + '\n' +
+                ", Szerkeztve=" + szerkeztve + "szerkesztes" + '\n' +
+                ", tartalom='" + tartalom +
+                '}';
+    }
 }
--- hu/petrik/BejegyzesProject/Main.java
+++ hu/petrik/BejegyzesProject/Main.java
@@ -8,5 +8,13 @@ public class Main {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        System.out.println(now.format(formatter));
+        LocalDateTime egyik = LocalDateTime.parse("2021-09-25 15:03:15", formatter);
+        LocalDateTime masik = LocalDateTime.parse("2021-09-25 15:03:15", formatter);
+        LocalDateTime harmadik = LocalDateTime.parse("2021-09-25 15:20:15", formatter);
+        if (egyik.equals(masik)) {
+            System.out.println("Ugyan az az idopont");
+        }
+
     }
 }
